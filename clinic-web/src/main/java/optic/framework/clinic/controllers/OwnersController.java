2cef1a50b471ad6aa74ce3aa38dfb8ff6ee51277
Abdul Azizi <saboorazizi.82@gmail.com>
2020-12-11 19:50:02 -0800

    Adding Owners Page and controller. closes #13

---
+++ clinic-web/src/main/java/optic/framework/clinic/controllers/OwnersController.java
@@ -0,0 +1,14 @@
+package optic.framework.clinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping({"/owners"})
+@Controller
+public class OwnersController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
