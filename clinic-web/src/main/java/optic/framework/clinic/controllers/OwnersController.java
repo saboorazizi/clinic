f83a539f3d516be162e4a860d233ab26a6773b37
Abdul Azizi <saboorazizi.82@gmail.com>
2020-12-12 22:39:49 -0800

    Adding Owners List and updated controller. closes #16

--- clinic-web/src/main/java/optic/framework/clinic/controllers/OwnersController.java
+++ clinic-web/src/main/java/optic/framework/clinic/controllers/OwnersController.java
@@ -1,14 +1,23 @@
 package optic.framework.clinic.controllers;
 
+import optic.framework.clinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"/owners"})
 @Controller
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
