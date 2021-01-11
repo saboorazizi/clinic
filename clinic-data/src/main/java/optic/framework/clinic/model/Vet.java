997b66eedcaf66844e16c9e2e0d28d19a57ba0d5
Abdul Azizi <saboorazizi.82@gmail.com>
2021-01-10 18:21:42 -0800

    Added specialties, added to vet. closes #27

--- clinic-data/src/main/java/optic/framework/clinic/model/Vet.java
+++ clinic-data/src/main/java/optic/framework/clinic/model/Vet.java
@@ -1,4 +1,15 @@
 package optic.framework.clinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
