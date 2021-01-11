e7ce0c50e6b47a1ffa68941ab35f07fb18582b91
Abdul Azizi <saboorazizi.82@gmail.com>
2021-01-10 19:29:42 -0800

    Added PetTypes to bootstrap. closes #32

--- clinic-web/src/main/java/optic/framework/clinic/bootstrap/DataLoader.java
+++ clinic-web/src/main/java/optic/framework/clinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package optic.framework.clinic.bootstrap;
 
 import optic.framework.clinic.model.Owner;
+import optic.framework.clinic.model.PetType;
 import optic.framework.clinic.model.Vet;
 import optic.framework.clinic.services.OwnerService;
+import optic.framework.clinic.services.PetTypeService;
 import optic.framework.clinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService){
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService){
         this.ownerService = ownerService;
         this.vetService = vetService;
+
+        this.petTypeService = petTypeService;
     }
     // implement methods
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
