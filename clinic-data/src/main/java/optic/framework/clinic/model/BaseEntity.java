445fccadd3d837ee70b9d9275376a5f386f8bc6e
Abdul Azizi <saboorazizi.82@gmail.com>
2021-01-18 22:34:45 -0800

    Update base entity with JPA annotation. closes #35

--- clinic-data/src/main/java/optic/framework/clinic/model/BaseEntity.java
+++ clinic-data/src/main/java/optic/framework/clinic/model/BaseEntity.java
@@ -1,10 +1,18 @@
 package optic.framework.clinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
     // Box type datatype recommended by Hibernate
     // instead of long we use Long
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
     public Long getId() {
