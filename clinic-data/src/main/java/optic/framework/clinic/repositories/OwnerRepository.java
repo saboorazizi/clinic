ee46bee3dfb1021c2bd79b73c445d794d587fba7
Abdul Azizi <saboorazizi.82@gmail.com>
2021-01-21 21:57:22 -0800

    Added spring data JPA service for owners. closes #40.

--- clinic-data/src/main/java/optic/framework/clinic/repositories/OwnerRepository.java
+++ clinic-data/src/main/java/optic/framework/clinic/repositories/OwnerRepository.java
@@ -4,5 +4,5 @@ import optic.framework.clinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
-
+    Owner findByLastName(String lastName);
 }
