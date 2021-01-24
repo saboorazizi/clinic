55dcce7f381287a1cb4d8573c3d046e139007db8
Abdul Azizi <saboorazizi.82@gmail.com>
2021-01-23 19:51:39 -0800

    add Specialty Spring Data JPA Service. closes #44.

---
+++ clinic-data/src/main/java/optic/framework/clinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,47 @@
+package optic.framework.clinic.services.springdatajpa;
+
+import optic.framework.clinic.model.Specialty;
+import optic.framework.clinic.repositories.SpecialtyRepository;
+import optic.framework.clinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJpaService implements SpecialtyService {
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties ;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
