35e0080673e7a9a36d3495bc6da9af1c17c5e645
Abdul Azizi <saboorazizi.82@gmail.com>
2021-03-18 17:48:00 -0700

    Adding builder pattern to PetType. Close #56

--- clinic-data/src/main/java/optic/framework/clinic/model/PetType.java
+++ clinic-data/src/main/java/optic/framework/clinic/model/PetType.java
@@ -15,6 +15,12 @@ import javax.persistence.Table;
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
     @Column(name = "name")
     private String name;
 
